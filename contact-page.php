4c7e5bd61dba77f21530b72d0f24bb045cfc2a18
ShtlMis <114349984+ShtlMis@users.noreply.github.com>
2024-03-07 13:44:29 +0100

    Update contact-page.php

--- contact-page.php
+++ contact-page.php
@@ -1,19 +1,19 @@
 <?php
 // database connection code
-if(isset($_POST['txtName']))
+if(isset($_POST['name']))
 {
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
 $con = mysqli_connect('localhost', 'root', '','db_songhai');
 
 // get the post records
 
-$txtName = $_POST['txtName'];
-$txtEmail = $_POST['txtCompany'];
-$txtPhone = $_POST['txtEmail'];
-$txtMessage = $_POST['txtMessage'];
+$phone = $_POST['phone'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
 
 // database insert SQL code
-$sql = "INSERT INTO 'tbl_test' ('fldPhone', 'fldName', 'fldEmail', 'fldOrganization', 'fldMessage') VALUES ('0', '$txtName', '$txtEmail', '$txtCompany', '$txtMessage')";
+$sql = "INSERT INTO 'tbl_test' ('fldPhone', 'fldName', 'fldEmail', 'fldMessage') VALUES ('0', 'name', 'email', 'message')";
 
 // insert in database 
 $rs = mysqli_query($con, $sql);
